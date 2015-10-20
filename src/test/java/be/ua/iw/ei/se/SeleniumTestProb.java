c0ef8a3ad70e93dfdc1130fe7ccaa908a11a0e44
kevinheyvaert <kevin-heyvaert@hotmail.com>
2015-10-20 18:56:40 +0200

    Selenium Test example

---
+++ src/test/java/be/ua/iw/ei/se/SeleniumTestProb.java
@@ -0,0 +1,27 @@
+package be.ua.iw.ei.se;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by Kevin on 20/10/2015.
+ */
+public class SeleniumTestProb {
+
+    @Test
+    public void startWebDriver() {
+        System.setProperty("webdriver.chrome.driver", "./chromedriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+
+        driver.navigate().to("http://146.175.138.153/");
+        Assert.assertFalse("body should start with this is a TEST" ,        //asserTrue proberen met Title
+                driver.getTitle().startsWith("This is a test"));
+
+        driver.close();
+        driver.quit();
+
+    }
+}
